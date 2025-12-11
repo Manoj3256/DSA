afb25d75b91f506e57bf609b8c4055b09a351c92
Manoj S <3256mmanoj@gmail.com>
2025-12-11 20:54:52 +0530

    Time: 107 ms (56.30%) | Memory: 18.1 MB (54.51%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def kClosest(self, points, k):
+        l=len(points)
+        a=[]
+        b=[]
+        t=0,
+        for i in range(l):
+            t=((points[i][0])**2+(points[i][1])**2)**0.5
+            a.append([t,i])
+        a.sort(reverse=False)
+        for i in range(k):
+            b.append(points[a[i][1]])
+        return b
