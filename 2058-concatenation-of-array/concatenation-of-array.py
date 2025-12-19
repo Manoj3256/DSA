6ad3c441a2a177a6466e8cae535b9d3f93ae9027
Manoj S <3256mmanoj@gmail.com>
2025-12-19 12:08:37 +0530

    Time: 4 ms (11.52%) | Memory: 12.8 MB (27.27%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans=[0]*(2*len(nums))
+        for i in range(len(nums)):
+            ans[i]=ans[i+len(nums)]=nums[i]
+        return ans
