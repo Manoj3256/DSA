8d5dac5c19d296184299665a655ee1b873427004
Manoj S <3256mmanoj@gmail.com>
2025-12-22 16:35:53 +0530

    Time: 27 ms (73.80%) | Memory: 12.8 MB (52.04%) - LeetSync

---
+++ 383-ransom-note/ransom-note.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        ran, maga={},{}
+        for i in  magazine:
+            if i in maga:
+                maga[i]+=1
+            else:
+                maga[i]=1
+        for i in ransomNote:
+            if i in maga:
+                if maga[i]>1:
+                    maga[i]-=1
+                else:
+                    maga.pop(i)
+            else:
+                return False
+        return True
