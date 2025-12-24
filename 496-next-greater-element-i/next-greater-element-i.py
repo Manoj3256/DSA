d3c7a6c85e72be895c8680216b7abbbb669823a1
Manoj S <3256mmanoj@gmail.com>
2025-12-24 20:07:10 +0530

    Time: 23 ms (30.78%) | Memory: 12.6 MB (54.82%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        dic,t,res={},0,[]
+        for i in range(len(nums2)-1):
+            count=i
+            while count<len(nums2):
+                if nums2[i]<nums2[count]:
+                    dic[nums2[i]]=nums2[count]
+                    break
+                
+                count+=1
+        print(dic)
+        for i in nums1:
+            if i in dic:
+                t=dic[i]
+                res.append(t)
+            else:
+                res.append(-1)
+        return res
