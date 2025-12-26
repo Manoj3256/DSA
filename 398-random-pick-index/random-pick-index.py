c514b3eb27c4fc9b8a3b08c38fca4e8fad02622e
Manoj S <3256mmanoj@gmail.com>
2025-12-26 19:24:45 +0530

    Time: 908 ms (5.22%) | Memory: 22.5 MB (86.96%) - LeetSync

---
+++ 398-random-pick-index/random-pick-index.py
@@ -0,0 +1,30 @@
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.dic={}
+        for i in range(len(nums)):
+            if nums[i]in self.dic:
+                self.dic[nums[i]].append(i)
+            else:
+                self.dic[nums[i]]=[i]
+        
+        print(self.dic)
+    def pick(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        import random as r
+        if target in self.dic:
+            l=len(self.dic[target])
+            j=r.randint(0,l-1)
+            return self.dic[target][j]
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
