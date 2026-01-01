2666bcf815c87539342be5e8f11135fd40609e70
Manoj S <3256mmanoj@gmail.com>
2026-01-01 19:28:55 +0530

    Time: 435 ms (6.03%) | Memory: 15.8 MB (19.43%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -5,21 +5,35 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        def rec(lis,k,s,result):
-            if not lis:
-                return result
-            curr,chance,left=next(iter(lis)), k,0
+        lis=set(s)
+        chance,result=k,0
+        for i in lis:
+            chance=k
+            left=0
             for right in range(len(s)):
-                if curr!=s[right]:
-                    
+                if i !=s[right]:
                     chance-=1
-                while chance < 0:
-                    if s[left] != curr:
+                while chance < 0 and left<right:
+                    if s[left] != i :
                         chance += 1
                     left += 1
-
                 result=max(result,right-left+1)
-            lis.remove(curr)
-            return rec(lis,k,s,result)
-        lis=set(s)
-        return rec(lis,k,s,0)
+        return result
+        # def rec(lis,k,s,result):
+        #     if not lis:
+        #         return result
+        #     curr,chance,left=next(iter(lis)), k,0
+        #     for right in range(len(s)):
+        #         if curr!=s[right]:
+                    
+        #             chance-=1
+        #         while chance < 0:
+        #             if s[left] != curr:
+        #                 chance += 1
+        #             left += 1
+
+        #         result=max(result,right-left+1)
+        #     lis.remove(curr)
+        #     return rec(lis,k,s,result)
+        # lis=set(s)
+        # return rec(lis,k,s,0)
