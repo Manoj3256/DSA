893161597b6ca7443a769abf9f0d94e7d279e428
Manoj S <3256mmanoj@gmail.com>
2026-01-01 19:38:12 +0530

    Time: 439 ms (5.99%) | Memory: 15.8 MB (19.43%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -6,10 +6,9 @@ class Solution(object):
         :rtype: int
         """
         lis=set(s)
-        chance,result=k,0
+        result=0
         for i in lis:
-            chance=k
-            left=0
+            chance,left=k,0
             for right in range(len(s)):
                 if i !=s[right]:
                     chance-=1
@@ -19,21 +18,3 @@ class Solution(object):
                     left += 1
                 result=max(result,right-left+1)
         return result
-        # def rec(lis,k,s,result):
-        #     if not lis:
-        #         return result
-        #     curr,chance,left=next(iter(lis)), k,0
-        #     for right in range(len(s)):
-        #         if curr!=s[right]:
-                    
-        #             chance-=1
-        #         while chance < 0:
-        #             if s[left] != curr:
-        #                 chance += 1
-        #             left += 1
-
-        #         result=max(result,right-left+1)
-        #     lis.remove(curr)
-        #     return rec(lis,k,s,result)
-        # lis=set(s)
-        # return rec(lis,k,s,0)
