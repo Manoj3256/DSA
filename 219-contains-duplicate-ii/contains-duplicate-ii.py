08114eb54a3afcf5b492d44fcd7c008b0d255c04
Manoj S <3256mmanoj@gmail.com>
2026-01-02 11:10:10 +0530

    Time: 55 ms (23.49%) | Memory: 25.8 MB (11.07%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        left,insidewin=0,set()
+        for right in range(len(nums)):
+            if nums[right] in insidewin:
+                return True
+            insidewin.add(nums[right])
+            if right-left >=k:
+                insidewin.remove(nums[left])
+                left+=1
+        return False
