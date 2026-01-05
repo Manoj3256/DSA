d98dbc24ef629bfc9b143a99d8c04ce1bf66bc30
Manoj S <3256mmanoj@gmail.com>
2026-01-05 14:07:37 +0530

    Time: 19 ms (5.84%) | Memory: 12.4 MB (84.35%) - LeetSync

--- 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
+++ 1987-substrings-of-size-three-with-distinct-characters/substrings-of-size-three-with-distinct-characters.py
@@ -7,17 +7,13 @@ class Solution(object):
         dic,count,left={},0,0
         for right in range(len(s)):
             dic[s[right]]=dic.get(s[right],0)+1
-            print("added",dic)
             if right-left+1==3:
-                print("entered to 3 window")
                 if len(dic)==3:
                     count+=1
                     print(count)
                 if dic[s[left]]>1:
                     dic[s[left]]=dic.get(s[left],0)-1
-                    print("deleted",s[left])
                 else:
-                    print("deleted",s[left])
                     del dic[s[left]]
                 left+=1
         return count
