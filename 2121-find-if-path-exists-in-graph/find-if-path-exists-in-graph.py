2b6a6be3481ae800072a926c4a3af92e580827ec
Manoj S <3256mmanoj@gmail.com>
2026-01-16 11:58:14 +0530

    Time: 1247 ms (40.28%) | Memory: 301.6 MB (33.19%) - LeetSync

---
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def validPath(self, n, edges, source, destination):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type source: int
+        :type destination: int
+        :rtype: bool
+        """
+        g=[[]for _ in range(n)]
+        for u,v in edges:
+            g[u].append(v)
+            g[v].append(u)
+        visited=[False]*n
+        
+        def dfs(root):
+            if root==destination:
+                return True
+            visited[root]=True
+            for i in g[root]:
+                if not visited[i]:
+                    if dfs(i):
+                        return True
+            return False
+        
+        return dfs(source)
