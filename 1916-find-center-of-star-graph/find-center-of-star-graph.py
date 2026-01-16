6f3c728b9fba0524472fed769626d382ca0bcefc
Manoj S <3256mmanoj@gmail.com>
2026-01-16 12:45:36 +0530

    Time: 0 ms (100.00%) | Memory: 33.2 MB (50.25%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.py
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.py
@@ -4,13 +4,7 @@ class Solution(object):
         :type edges: List[List[int]]
         :rtype: int
         """
-        maxval = 0
-        for u,v in edges:
-            maxval=max(maxval, u, v)
-        g = [[]for _ in range(maxval + 1)]
-        for u,v in edges:
-            g[u].append(v)
-            g[v].append(u)
-        for i in range(1,len(g)):
-            if len(g[i])==len(edges):
-                return i
+        (a,b),(c,d)=edges[0],edges[1]
+        if a==c or a==d:
+            return a
+        return b
