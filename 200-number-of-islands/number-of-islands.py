6f3b18257507b9afa2d41fd1fe7491cb0f650a8d
Manoj S <3256mmanoj@gmail.com>
2026-01-18 20:42:52 +0530

    Time: 211 ms (89.76%) | Memory: 27.6 MB (95.19%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,21 +4,21 @@ class Solution(object):
         :type grid: List[List[str]]
         :rtype: int
         """
-        if not grid:
-            return 0
-        rows,cols=len(grid),  len(grid[0])
-        count=0
-        def dfs(r,c):
-            if r<0 or c<0 or r>=rows or c>=cols or grid[r][c]=="0":
-                return
-            grid[r][c]="0"  
-            dfs(r+1,c)
-            dfs(r-1,c)
-            dfs(r,c+1)
-            dfs(r,c-1)
-        for i in range(rows):
-            for j in range(cols):
-                if grid[i][j] == "1":
-                    count+= 1
-                    dfs(i,j)
-        return count
+        from collections import deque
+        if not grid: return 0
+        R,C,ans =len(grid),len(grid[0]),  0
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c]=="1":
+                    ans+=1
+                    q=deque([(r,c)])
+                    grid[ r][c]="0"
+                    while q:
+                        x,y=q.popleft()
+                        for dx ,dy in ((1,0),(-1,0),(0,1),(0,-1)):
+                            nx,ny =x+dx,y+dy
+                            if 0<=nx<R and 0<=ny<C and grid[nx][ny]=="1":
+                                grid[nx][ny]="0"
+                                q.append((nx,ny))
+        return ans
+
