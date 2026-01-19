7f0a0bbb80272be504985629ebc37c64f24fdab4
Manoj S <3256mmanoj@gmail.com>
2026-01-19 22:21:05 +0530

    Time: 3 ms (93.72%) | Memory: 12.8 MB (90.71%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def findCircleNum(self, isConnected):
+        """
+        :type isConnected: List[List[int]]
+        :rtype: int
+        """
+        from collections import  deque
+        n=len(isConnected)
+        visited=set( )
+        result=0
+        for i in range(n):
+            if i not in visited:
+                q = deque([i])
+                visited.add(i)
+                while q:
+                    item=q.popleft()
+                    for j in range(n):
+                        if isConnected[item][j] == 1 and j not in visited:
+                            visited.add(j)
+                            q.append(j)
+                result+=1
+        return result
+
+        
